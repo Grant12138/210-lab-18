ba98974180ea31591ca5d3e081f543af9b45b537
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-10-05 12:53:00 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -17,18 +17,83 @@ struct Review
 };
 
 // Function prototypes
+Review* getReview(Review* &);
 int getChoice();
+void stackReview();
+void queueReview();
 
 int main()
 {
+    Review* head = nullptr;
+    Review* aReview = getReview(head);
+
     cout << "Which linked list method should we use?\n";
     cout << "    [1] New nodes are added at the head of the linked list\n";
     cout << "    [2] New nodes are added at the tail of the linked list\n";
+    int choice = getChoice();
+
+    if (choice == 1)
+        stackReview();
+    else
+        queueReview();
 
     return 0;
 }
 
+Review* getReview(Review* &head)
+{
+    Review* newReview = new Review();
+
+    // Get review rating
+    while (true)
+    {
+        try
+        {
+            cout << "Enter review rating 0 - 5: ";
+            string buf;
+            cin >> buf;
+            cin.ignore(1000, '\n');
+            newReview->rating = stod(buf);
+            if (newReview->rating >= 0.0 && newReview->rating <= 5.0)
+                break;
+            cout << "Please enter a value between 0 and 5\n";
+        }
+        catch (invalid_argument &)
+        {
+            cout << "You did not enter a valid rating\n";
+        }
+    }
+
+    // Get review comments
+    cout << "Enter review comments: ";
+    getline(cin, newReview->comments);
+
+    return newReview;
+}
+
 int getChoice()
 {
-    cout << "    Choice: ";
+    while (true)
+    {
+        try
+        {
+            cout << "    Choice: ";
+            string buf;
+            cin >> buf;
+            cin.ignore(1000, '\n');
+            int choice = stoi(buf);
+            if (choice == 1 || choice == 2)
+                return choice;
+            cout << "Enter either 1 or 2 for your choice\n";
+        }
+        catch (invalid_argument &)
+        {
+            cout << "You did not enter a valid choice\n";
+        }
+    }
+}
+
+void stackReview(Review* &head, Review* aReview)
+{
+
 }
